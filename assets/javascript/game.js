8a0630bd59d831e4a5e3d49de2bf9aa505b49fc9
Sean Huff <swhufnagel@gmail.com>
2019-04-11 17:13:16 -0600

    fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,12 +21,12 @@ var crystals = {
         }
 
     },
-    randomize: function(){
-        crystals.pink = Math.floor(Math.random() * (12-1) +1);
-        crystals.green = Math.floor(Math.random() * (12-1)+1);
-        crystals.purple = Math.floor(Math.random() * (12-1)+1);
-        crystals.blue = Math.floor(Math.random() * (12 -1)+1);
-}
+    randomize: function () {
+        crystals.pink = Math.floor(Math.random() * (12 - 1) + 1);
+        crystals.green = Math.floor(Math.random() * (12 - 1) + 1);
+        crystals.purple = Math.floor(Math.random() * (12 - 1) + 1);
+        crystals.blue = Math.floor(Math.random() * (12 - 1) + 1);
+    }
 }
 var game =
 {
@@ -37,69 +37,81 @@ var game =
         score.text("Your Score: " + counter.usrScore);
         $('#playScore').append(score);
         var wins = $('<p>');
+        wins.addClass('wins');
         wins.text("Wins: " + counter.wins);
         $('#score').append(wins);
         var losses = $('<p>')
+        losses.addClass('losses');
         losses.text("Losses: " + counter.losses);
         $('#score').append(losses);
     },
-    reset: function(){
+    reset: function () {
         crystals.randomize();
-        counter.targScore = Math.floor(Math.random() * (120 - 19)+19);
+        counter.targScore = Math.floor(Math.random() * (120 - 19) + 19);
         counter.usrScore = 0;
+        $('#playScore').html("Your Score: " + counter.usrScore);
+        $('#target').html("Target Score: " + counter.targScore);
+        $('.wins').html("Wins: " + counter.wins);
+        $('.losses').html("Losses: " + counter.losses);
     }
 }
 $(document).ready(function () {
     // if not playing show play button
-    if (game.play === false) {
-        var play = $('<button>');
-        play.addClass("playAgain")
-        play.text("Play");
-        $('#playScore').append(play);
-    }
-    //if play button is pressed start game
-    $('.playAgain').click(function () {
-        counter.targScore = Math.floor(Math.random() * (120 - 19)+19);
-        $('#target').append("Target Score: " + counter.targScore)
-        $("#instr").addClass("hidePlay");
-        game.play = true;
-        game.scoreboard();
-        play.addClass("hidePlay");
-        $(".scoreboard").addClass("showPlay");
-        crystals.draw();
-        crystals.randomize();
-        //if each crystal is clicked
-        $('#img0').click(function () {
-            counter.usrScore += crystals.pink;
-            $(".scoreboard").text("Your Score: " + counter.usrScore);
-        })
-        $('#img1').click(function () {
-            counter.usrScore += crystals.green;
-            $(".scoreboard").text("Your Score: " + counter.usrScore);
-        })
-        $('#img2').click(function () {
-            counter.usrScore += crystals.purple;
-            $(".scoreboard").text("Your Score: " + counter.usrScore);
-        })
-        $('#img3').click(function () {
-            counter.usrScore += crystals.blue;
-            $(".scoreboard").text("Your Score: " + counter.usrScore);
-            
-        })
-        if(counter.usrScore === counter.targScore){
-            alert("You Win!");
-            counter.wins++;
-            game.reset();
+        if (game.play === false) {
+            var play = $('<button>');
+            play.addClass("playAgain")
+            play.text("Play");
+            $('#playScore').append(play);
         }
-        if(counter.usrScore > counter.targScore){
-            alert("You Lose!");
-            counter.losses++;
-            game.reset();
+        function winCheck() {
+            if (counter.usrScore === counter.targScore) {
+                alert("You Win!");
+                counter.wins++;
+                game.reset();
+                game.play = false;
+
+            }
+            else if (counter.usrScore > counter.targScore) {
+                alert("You Lose!");
+                counter.losses++;
+                game.reset();
+                game.play = false;
+
+            }
         }
-    })
-
-    
-
-
-
+        //if play button is pressed start game
+        $('.playAgain').click(function () {
+            counter.targScore = Math.floor(Math.random() * (120 - 19) + 19);
+            $('#target').html("Target Score: " + counter.targScore);
+            $("#instr").addClass("hidePlay");
+            game.play = true;
+            game.scoreboard();
+            play.addClass("hidePlay");
+            $(".scoreboard").addClass("showPlay");
+            crystals.draw();
+            crystals.randomize();
+            //if each crystal is clicked
+            $(document).on("click", "#img0", function () {
+                counter.usrScore += crystals.pink;
+                $("#playScore").html("Your Score: " + counter.usrScore);
+                winCheck();
+            })
+            $(document).on("click", "#img1", function () {
+                counter.usrScore += crystals.green;
+                console.log("you clicked green");
+                console.log("score: " + counter.usrScore);
+                $("#playScore").html("Your Score: " + counter.usrScore);
+                winCheck();
+            })
+            $(document).on("click", "#img2", function () {
+                counter.usrScore += crystals.purple;
+                $("#playScore").html("Your Score: " + counter.usrScore);
+                winCheck();
+            })
+            $(document).on("click", "#img3", function () {
+                counter.usrScore += crystals.blue;
+                $("#playScore").html("Your Score: " + counter.usrScore);
+                winCheck();
+            })
+        })
 })
